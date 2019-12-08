296d91c4ca013e34c5ddb8d2c3b4143906188e73
francisco martinez <jua.martinez@outlook.es>
2019-12-08 02:07:55 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+// variables
+const listTweets = document.getElementById('lista-tweets');
+
+//event listeners
+
+escucha();
+function escucha (){
+    //cuando se envia el formulario
+    document.getElementById('formulario').addEventListener('submit', agregarTweet);
+
+};
+
+//funciones
+
+//añadir tweet al formulario
+
+function agregarTweet(e){
+    e.preventDefault();
+    //leer el valor de textarea
+    const tweet = document.getElementById("tweet").value;
+    //crear list
+    const li = document.createElement('li');
+    //boton borrar
+    const borrar = document.createElement('a');
+    borrar.classList = 'borrar-tweet';
+    borrar.innerText = 'X'
+    li.innerText = tweet;
+    li.appendChild(borrar);
+    listTweets.appendChild(li);
+    console.log(tweet);
+}
+
+//
+
