d94fa1a396cf9d14b7c44a9416e5adfcb85393ab
francisco martinez <jua.martinez@outlook.es>
2019-12-14 02:26:26 -0300

    arreglar local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
 // variables
 const listTweets = document.getElementById('lista-tweets');
 
+const tweet = document.querySelector('#tweet')
 //event listeners
 
 escucha();
 function escucha (){
     //cuando se envia el formulario
     document.getElementById('formulario').addEventListener('submit', agregarTweet);
+
+    listTweets.addEventListener('click', borrarTweet)
 
 };
 
@@ -17,25 +20,50 @@ function escucha (){
 function agregarTweet(e){
     e.preventDefault();
     //leer el valor de textarea
+    // @ts-ignore
     const tweet = document.getElementById("tweet").value;
-    //crear list
+    //crear lista de tweets
     const li = document.createElement('li');
-    //boton borrar
-    const borrar = document.createElement('a');
-    borrar.classList = 'borrar-tweet';
-    borrar.innerText = 'X'
-    li.innerText = tweet;
-    li.appendChild(borrar);
-    listTweets.appendChild(li);
-    console.log(tweet);
+        //boton borrar
+        const borrar = document.createElement('a');
+        borrar.className = 'borrar-tweet';
+        borrar.innerText = 'X'
+        //texto al Elemento LI
+        li.innerText = tweet;
+        li.appendChild(borrar);
+        listTweets.appendChild(li);
+        //funcion que añade al local storage ()=>
+        agregarTweetLocalStorage(tweet);
 }
-
+//elminar tweet Del Dom 
 function borrarTweet(e){
     e.preventDefault();
-    if(e.target.className === 'borrar-tweet'){
-        console.log(e.target.parentElement.remove());
-        alert('tweet Eliminado');
-    }
+    e.target.parentElement.remove();
+    removeLocal(tweet)
 }
-//
+
+function removeLocal(tweet){
+    tweet.preventDefault();
+    localStorage.removeItem('tweets');
+};
+//Agregar Tweee a localStorage
+function agregarTweetLocalStorage(tweet){
+    let tweets;
+    tweets = obtenerTweetLocalStorage();
+    tweets.push(tweet);
+    localStorage.setItem('tweets', JSON.stringify(tweets))
+}
+
+//remover data desde LocalStorage
+function obtenerTweetLocalStorage(tweet){
+    let tweets;
+    //Revision los valores en local storge
+    if(localStorage.getItem('tweet')===null){
+        tweets=[]
+        return alert(' no existen tweets ') //al no encontrar elementos lo devuelve como arreglo vacio 
+    }else{
+        tweets=JSON.parse(localStorage.getItem('tweet'));
+    }
+    return tweets;
+}
 
